e64f91ba92230ba707acef60d0c67b7e24a21e93
Morna Carrick <30137568@cityofgla.ac.uk>
2018-04-08 22:43:42 +0100

    Add project files.

---
+++ OpenPropViewing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenPropViewing.Startup))]
+namespace OpenPropViewing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
